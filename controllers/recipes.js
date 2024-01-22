2059ee4707cec0631273c6f569f74bb2512713df
joshuabutler <butlerja.nhs@gmail.com>
2024-01-22 10:55:34 -0500

    create new recipe view

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -14,6 +14,14 @@ function index(req, res) {
   })
 }
 
+function newRecipe (req, res) {
+  res.render('recipes/new', {
+    title: 'Add Recipe'
+  })
+}
+
 export {
   index,
+  newRecipe as new,
+  
 }
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import * as recipesCtrl from '../controllers/recipes.js'
+import { isLoggedIn } from "../middleware/middleware.js"
 
 const router = Router()
 
 // GET localhost:3000/recipes
 router.get('/', recipesCtrl.index)
+// GET localhost:3000/recipes/new
+router.get('/new', isLoggedIn, recipesCtrl.new)
 
 export { router }
