dc5472acba29935b2b595ea7af00028dbcda4628
joshuabutler <butlerja.nhs@gmail.com>
2024-01-22 16:48:45 -0500

    restructure new views

--- routes/ingredients.js
+++ routes/ingredients.js
@@ -3,7 +3,8 @@ import * as ingredientsCtrl from '../controllers/ingredients.js'
 
 const router = Router()
 
-
+// GET localhost:3000/ingredients/new
+router.get('/new', ingredientsCtrl.new)
 
 export {
   router
