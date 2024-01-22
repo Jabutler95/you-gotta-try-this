c1b62067e44b806b7fc92e79b1b6611ddcac6c25
joshuabutler <butlerja.nhs@gmail.com>
2024-01-22 09:19:09 -0500

    create recipe schema

---
+++ models/recipe.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const recipeSchema =  new Schema({
+  name: String, 
+  ingredients: [ingredientsSchema],
+  instructions: String
+})
+
+const Recipe = mongoose.model('Recipe', recipeSchema)
+
+export {
+  Recipe
+}
