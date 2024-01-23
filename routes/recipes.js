0fd5caf0cf62e892038350ed791d49c27db10181
joshuabutler <butlerja.nhs@gmail.com>
2024-01-23 09:54:11 -0500

    create route for edit

--- routes/recipes.js
+++ routes/recipes.js
@@ -10,6 +10,8 @@ router.get('/', recipesCtrl.index)
 router.get('/new', isLoggedIn, recipesCtrl.new)
 // GET localhost:3000/recipes/show
 router.get('/:recipeId', isLoggedIn, recipesCtrl.show)
+// GET localhost:3000/recipes/:recipeId/edit
+router.get('/:recipeId/edit', recipesCtrl.edit)
 // POST localhost:3000/recipes
 router.post('/', isLoggedIn, recipesCtrl.create)
 
