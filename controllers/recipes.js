1b8b6dc8936a89af35141460de5b2077a87fd4f7
joshuabutler <butlerja.nhs@gmail.com>
2024-01-24 12:10:56 -0500

    add new controller for creating recipes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -17,33 +17,56 @@ function index(req, res) {
 
 function newRecipe(req, res) {
   Ingredient.find({})
-    .then(ingredients => {
-      res.render('recipes/new', {
-        title: 'Add Recipe',
-        ingredients: ingredients  
-      })
+  .then(ingredients => {
+    res.render('recipes/new', {
+      title: 'Add Recipe',
+      ingredients: ingredients  
     })
-    .catch(err => {
-      console.log(err);
-      res.redirect('/recipes/new');
-    })
+  })
+  .catch(err => {
+    console.log(err);
+    res.redirect('/recipes/new');
+  })
 }
 
 
-function create(req, res) {
-  Recipe.create(req.body)
-  .then(recipe => {
+// function create(req, res) {
+//   Recipe.create(req.body)
+//   .then(recipe => {
+//     res.redirect('/recipes')
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.redirect('/recipes/new')
+//   })
+// }
+
+async function makeNewRecipe(req, res) {
+  try {
+    const ingredientIdList = await addIngredients(req, res)
+    req.body.ingredients = ingredientIdList
+    const recipe = await Recipe.create(req.body)
+    res.redirect(`/recipes/${recipe._id}`)
+  } catch (error) {
+    console.log(error)
     res.redirect('/recipes')
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/recipes/new')
-  })
+  }
+}
+
+async function addIngredients(req, res) {
+  const ingredientIdList = []
+  for (ingredient in req.body.ingredientsToAdd) {
+    const ingredientToPush = await Ingredient.create(ingredient)
+    ingredientIdList.push(ingredientToPush._id)
+  }
+  return ingredientIdList
 }
 
 function show(req, res) {
   Recipe.findById(req.params.recipeId)
-  .populate('ingredients')
+  .populate([
+    {path: "ingredients"}
+  ])
   .then(recipe => {
     Ingredient.find(req.params.ingredientId)
     .then(ingredients => {
@@ -109,29 +132,29 @@ function deleteRecipe(req, res) {
   })
 }
 
-function addIngredients(req, res) {
-  Recipe.findById(req.params.recipeId)
-  .then(recipe => {
-    recipe.ingredients.push(req.body.ingredientId)
-    recipe.save()
-    .then(() => {
-      res.redirect(`/recipes/${recipe._id}`)
-    })
-    .catch(err => {
-      console.log(err)
-      res.redirect('/recipes')
-    })
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/recipes')
-  })
-}
+// function addIngredients(req, res) {
+//   Recipe.findById(req.params.recipeId)
+//   .then(recipe => {
+//     recipe.ingredients.push(req.body.ingredientId)
+//     recipe.save()
+//     .then(() => {
+//       res.redirect(`/recipes/${recipe._id}`)
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.redirect('/recipes')
+//     })
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.redirect('/recipes')
+//   })
+// }
 
 export {
   index,
   newRecipe as new,
-  create,
+  makeNewRecipe as create,
   show,
   edit,
   update,
--- models/recipe.js
+++ models/recipe.js
@@ -3,8 +3,18 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+// const ingredientsSchema = new Schema({
+//   ingredientName: String,
+//   quantity: String, 
+//   unit: {
+//     type: String, 
+//     enum: ['millileter(s)', 'liter(s)', 'deciliter(s)', 'teaspoon(s)', 'tablespoon(s)', 'fluid ounce(s)', 'gill(s)', 'cup(s)', 'pint(s)', 'quart(s)', 'gallon(s)', 'milligram(s)', 'gram(s)', 'kilogram(s)', 'pound(s)', 'ounce(s)']
+//   }
+// })
+
 const recipeSchema =  new Schema({
   recipeName: String, 
+  // ingredients: [ingredientsSchema]
   ingredients: [{type: Schema.Types.ObjectId, ref: 'Ingredient'}],
   instructions: String
 })
