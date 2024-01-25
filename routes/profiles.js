7b8d9c9c7ed1ea31346a0f0a68b4bd7b86db4512
joshuabutler <butlerja.nhs@gmail.com>
2024-01-25 09:30:35 -0500

    write show route for profile

---
+++ routes/profiles.js
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
+import * as profilesCtrl from '../controllers/profiles.js'
+const router = Router()
+
+// GET localhost:3000/profiles/:profileId
+router.get('/:profileId', isLoggedIn, profilesCtrl.show)
