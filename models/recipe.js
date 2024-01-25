6e4275f01e22d3546a6d7d8bd8aeb3bdfbaca460
joshuabutler <butlerja.nhs@gmail.com>
2024-01-25 08:41:48 -0500

    build comment schema

--- models/recipe.js
+++ models/recipe.js
@@ -3,12 +3,19 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
-
+const commentSchema = new Schema({
+  content: String, 
+}, {
+  timestamps: true
+})
 
 const recipeSchema =  new Schema({
   recipeName: String, 
   ingredients: [{type: Schema.Types.ObjectId, ref: 'Ingredient'}],
-  instructions: String
+  instructions: String,
+  comments: [commentSchema]
+}, {
+  timestamps: true
 })
 
 const Recipe = mongoose.model('Recipe', recipeSchema)
